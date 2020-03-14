4acf50836431757a856eea11e42fed961093b98c
blake <blake.wang@mic.com.tw>
2020-03-14 18:57:55 +0800

    2020 0314 home

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import get_template
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
+
+def homepage(request):
+    template = get_template('login.html')
+    # LoginResult = "1"
+    html = template.render(locals())
+    return HttpResponse(html)
